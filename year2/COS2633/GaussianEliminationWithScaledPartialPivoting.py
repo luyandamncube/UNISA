2ce1b915875c662faa9e7e9478f5775142a5cb29
luyandamncube <lmn3333@gmail.com>
2021-07-24 23:10:44 +0200

    Finished Jacobi Method. Added support for b vector

--- year2/COS2633/GaussianEliminationWithScaledPartialPivoting.py
+++ year2/COS2633/GaussianEliminationWithScaledPartialPivoting.py
@@ -4,63 +4,74 @@ import pandas as pd
 np.set_printoptions(formatter={'float': lambda x: "{0:0.4f}".format(x)})
 
 seperator = '-----------------------------------------------------'
-def printArray(array,precision):
-    df=pd.DataFrame(data=array[0:,0:],
-                    index=[i+1 for i in range(array.shape[0])],
-                    columns=['x'+str(i+1) for i in range(array.shape[1])])
+def isSquare (m): 
+    return all (len (row) == len (m) for row in m)
+
+def printArray(A, b, precision):
+    df=pd.DataFrame(data=A[0:,0:],
+                    index=[i+1 for i in range(A.shape[0])],
+                    columns=['x'+str(i+1) for i in range(A.shape[1])])
+    if isSquare(A):
+        df['b'] = b
+    else:
+        df.rename(columns = {'x5':'b'}, inplace = True)
 
     pd.set_option("display.precision", precision)
     if (precision > 2):
         pd.set_option('display.float_format', lambda x: "{0:0.4f}".format(x))
     print(df)
 
-def getInput():
-    # a=np.array([
-    #     [0.06, 0.08, 0.07, 0.08, 0.29],
-    #     [0.08, 0.20, 0.09, 0.07, 0.44],
-    #     [0.07, 0.09, 0.20, 0.10, 0.46],
-    #     [0.06, 0.08, 0.10, 0.20, 0.44],
-    # ], dtype=float)
-
-    a=np.array([
-        [0.05, 0.07, 0.06, 0.05, 0.23],
-        [0.07, 0.10, 0.08, 0.07, 0.32],
-        [0.06, 0.08, 0.10, 0.09, 0.33],
-        [0.05, 0.07, 0.09, 0.10, 0.31],
+def getInputA(b=None):    
+    A=np.array([
+        [0.05, 0.07, 0.06, 0.05],
+        [0.07, 0.10, 0.08, 0.07],
+        [0.06, 0.08, 0.10, 0.09],
+        [0.05, 0.07, 0.09, 0.10],
     ], dtype=float)
-    return a
+    
+    Ab=[]
+    if len(b) != 0:
+        for i in range(A.shape[0]):
+            Ab.append(np.append(A[i], b[i]))
+            print(A[i])
+    return (A,Ab)
 
-def getRowsColumns(array):
+def getInputB():
+    # initialize the RHS vector
+    b = np.array([0.23, 0.32, 0.33, 0.31])
+    return b
+
+def getRowsColumns(Ab):
     #get rows and columns
-    rows=np.shape(a)[0]
-    cols=np.shape(a)[1]
+    rows=np.shape(Ab)[0]
+    cols=np.shape(Ab)[1]
     x=np.zeros(cols-1)
     return(rows,cols,x)
 
-def forwardElimination(array,rows,cols):
-    
-    scale = [max([abs(array[i][j]) for j in range(rows)]) for i in range(rows)] 
+def forwardElimination(A,rows,cols):
+    Ab=np.array([np.array(xi) for xi in A])
+    scale = [max([abs(Ab[i][j]) for j in range(rows)]) for i in range(rows)] 
         
     # number of steps: n -1
     for i in range (cols-1):
         print(f'\nS= {scale}')
-        pivot(array, rows, i, scale)
+        pivot(Ab, rows, i, scale)
         # print("\n")            
         if i < cols-2:
             for j in range (i+1,rows):
-                numerator = round(a[j,i],4)
-                denominator = round(a[i,i],4)    
+                numerator = round(Ab[j,i],4)
+                denominator = round(Ab[i,i],4)    
                 if (numerator != 0 and denominator !=0):
                     print(f'R{j+1}: R{j+1} - ({numerator})/({denominator}) * R{i+1}')
-                    a[j,:] = a[j,:] - (numerator/denominator)*a[i,:]
+                    Ab[j,:] = Ab[j,:] - (numerator/denominator)*Ab[i,:]
             print(f"\n[{i+2}] {seperator}")
-            printArray(a,4)
+            printArray(Ab,None,4)
             print("\n")
             
-    if array[rows-1][rows-1] == 0: 
+    if A[rows-1][rows-1] == 0: 
         print('No unique solution')
         exit()
-    return a     
+    return Ab    
 
 def backSubstitution(array,x,cols,rows):
     # arange: start from a particular point
@@ -90,14 +101,13 @@ def pivot(array, rows, i, scale):
   print(f"max:{max}\n")
   
 if __name__ == "__main__":
-    a = getInput()
+    b = getInputB()
+    A, Ab = getInputA(b)
     print(f"\nFORWARD ELIMINATION: \nrow echelon form")
-    # print('\n'+seperator)
     print(f"\n[{1}] {seperator}")
-    printArray(a,2)
-    rows,cols,x = getRowsColumns(a)
-    # print(f"\nrows: {rows}, cols {cols}, x {x}")
+    printArray(A, b, 2)
+    rows,cols,x = getRowsColumns(Ab)
     
  
-    a = forwardElimination(a,rows,cols)
-    backSubstitution(a,x,cols,rows)
+    Ab = forwardElimination(Ab,rows,cols)
+    # backSubstitution(a,x,cols,rows)
---
+++ year2/COS2633/JacobiMethod.py
@@ -0,0 +1,93 @@
+#https://en.wikipedia.org/wiki/Jacobi_method
+import numpy as np
+import pandas as pd
+
+ITERATION_LIMIT = 4
+num_arr = [0,1,2,3]
+seperator = '-----------------------------------------------------'
+
+def printArray(A, b, precision):
+    df=pd.DataFrame(data=A[0:,0:],
+                    index=[i+1 for i in range(A.shape[0])],
+                    columns=['x'+str(i+1) for i in range(A.shape[1])])
+
+    df['b'] = b
+    pd.set_option("display.precision", precision)
+    if (precision > 2):
+        pd.set_option('display.float_format', lambda x: "{0:0.4f}".format(x))
+    print(df)
+
+def getInputA():
+    # a=np.array([
+    #     [0.06, 0.08, 0.07, 0.08, 0.29],
+    #     [0.08, 0.20, 0.09, 0.07, 0.44],
+    #     [0.07, 0.09, 0.20, 0.10, 0.46],
+    #     [0.06, 0.08, 0.10, 0.20, 0.44],
+    # ], dtype=float)
+    
+    # initialize the matrix
+    A = np.array([[0.06, 0.08, 0.07, 0.08],
+                [0.08, 0.20, 0.09, 0.07],
+                [0.07, 0.09, 0.20, 0.10],
+                [0.06, 0.08, 0.10, 0.20]])
+    return A
+
+def getInputB():
+    # initialize the RHS vector
+    b = np.array([0.29, 0.44, 0.46, 0.44])
+    return b
+
+def diagDominant(A):
+    D = np.diag(np.abs(A)) # Find diagonal coefficients
+    S = np.sum(np.abs(A), axis=1) - D # Find row sum without diagonal
+    if np.all(D > S):
+        print('matrix is diagonally dominant')
+    else:
+        print('NOT diagonally dominant')
+
+def jacobiMethod(A, b):
+    x = np.zeros_like(b)
+    # print(f'x: {x}')
+    ans_arr = []
+    for it_count in range(ITERATION_LIMIT):
+        if it_count != 0:
+            print(seperator)
+            print(f"Iteration {it_count}: {x}\n")
+        x_new = np.zeros_like(x)
+        
+        ans_arr.append(x)
+        for i in range(A.shape[0]):
+            # lower triangle of matrix A[i, :i]
+            # upper triangle of matrix A[i, i + 1:]
+            s1 = np.dot(A[i, :i], x[:i])
+            s2 = np.dot(A[i, i + 1:], x[i + 1:])
+
+            temp_arr = list(A[i, :i])+ list(A[i, i + 1:])
+            num_arr.remove(i)
+            if it_count != 0:
+                print(f"x_{i+1} = 1/{A[i,i]} * [{b[i]}  - {temp_arr[0]} * {round(ans_arr[it_count-1][num_arr[0]],5)} - {temp_arr[1]} * {round(ans_arr[it_count-1][num_arr[1]],5)} - {temp_arr[2]} * {round(ans_arr[it_count-1][num_arr[2]],5)} ] = {round(x[i],5)}")
+            x_new[i] = (b[i] - s1 - s2) / A[i, i]
+            num_arr.append(i)
+            num_arr.sort()
+
+            if x_new[i] == x_new[i-1]:
+                break
+
+        if np.allclose(x, x_new, atol=1e-10, rtol=0.):
+            break
+        x = x_new
+    return (x)
+
+if __name__ == "__main__":
+    A = getInputA()    
+    b = getInputB()
+    diagDominant(A)
+
+    printArray(A, b, 2)
+    x = jacobiMethod(A, b)
+    print(seperator)
+    print("Solution:")
+    print(x)
+    error = np.dot(A, x) - b
+    print("Error:")
+    print(error)
