e36fbd078d62ab73f20fdc488cd2ab20a40dcc6d
luyandamncube <lmn3333@gmail.com>
2021-07-27 22:30:43 +0200

    Finished GaussSiedel for 3 digits

--- year2/COS2633/GaussSiedel.py
+++ year2/COS2633/GaussSiedel.py
@@ -2,10 +2,13 @@
 import pandas as pd
 import numpy as np
 
-x = [0.00, 0.00, 0.00]  
+# CHANGE THIS FOR 3 DIGITS
+x = [0.00, 0.00, 0.00, 0.00]  
+# x = [3.00, 0.00, 3.00, 1.00] 
 ITERATION_LIMIT = 25
 seperator = '-----------------------------------------------------'
-num_arr = [0,1,2]
+# CHANGE THIS FOR 3 DIGITS
+num_arr = [0,1,2,3]
 temp_arr = []
 
 def printArray(A, b, precision):
@@ -21,20 +24,23 @@ def printArray(A, b, precision):
     print(df)
 
 def seidel(a, x ,b):    
-    n = len(a)                   
+    n = len(a)    
+    print(f'LEN: {n}')               
     for i in range(0, ITERATION_LIMIT):   
         print(f"iteration {i+1}: {seperator}")   
         for j in range(0, n):        
             d = b[j] 
             num_arr.remove(j)  
             for k in range(0, n):  
+                # print(k)
                 if(j != k):
                     d-=a[j][k] * x[k]     
             x[j] = d / a[j][j]
             temp = b[j]
-            if (j == 1 and i > 0):
-                temp = x[0]
-            print(f"x_{j+1} = 1/{a[j][j]} × ({round(temp,5)} - {a[j][num_arr[0]]} × {round(x[num_arr[0]],5)} - {a[j][num_arr[1]]} × {round(x[num_arr[1]],5)}) = {round(x[j],5)}")   
+            # ADD THIS IN FOR 3 DIGITS
+            # if (j == 1 and i > 0):
+            #     temp = x[0]
+            print(f"x_{j+1} = 1/{a[j][j]} × ({round(temp,5)} - {a[j][num_arr[0]]} × {round(x[num_arr[0]],5)} - {a[j][num_arr[1]]} × {round(x[num_arr[1]],5)} - {a[j][num_arr[2]]} × {round(x[num_arr[2]],5)}) = {round(x[j],5)}")   
             num_arr.append(j)
             num_arr.sort()        
        
@@ -42,15 +48,30 @@ def seidel(a, x ,b):
 def getInputA():    
     # initialize the matrix
     a = np.array([
-        [0.25, 0.1, -0.35],
-        [0.5, 0.40, 0.10],
-        [0.25, 0.5, 0.25]
+        [0.05, 0.07, 0.06, 0.05],
+        [0.07, 0.10, 0.08, 0.07],
+        [0.06, 0.08, 0.10, 0.09],
+        [0.05, 0.07, 0.09, 0.10],
     ], dtype=float)
+    # a = np.array([
+    #     [0.06, 0.08, 0.07, 0.08],
+    #     [0.08, 0.20, 0.09, 0.07],
+    #     [0.07, 0.09, 0.20, 0.10],
+    #     [0.06, 0.08, 0.10, 0.20],
+    # ], dtype=float)
+    # a = np.array([
+    #     [2.141, -2.718, 1.414, -1.732],
+    #     [9.869, 2.718, -7.389, 0.428],
+    #     [2.236, -2.449, 1, -1.414],
+    #     [31.006, 7.389, -2.645, 0.111],
+    # ], dtype=float)
     return a
 
 def getInputB():
     # initialize the RHS vector
-    b = np.array([0.9, 0.6, 0.5], dtype=float)
+    b = np.array([0.23, 0.32, 0.33, 0.31], dtype=float)
+    # b = np.array([3.316, 0 , 3.141, 1.414], dtype=float)
+    # b = np.array([0.29, 0.44, 0.46, 0.44], dtype=float)
     return b
 
 if __name__ == "__main__":
